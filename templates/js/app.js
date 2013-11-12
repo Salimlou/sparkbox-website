ff6ba2d20291e81a4951c68e67c489ade409aa34
zastrow <philip@phanza.com>
2013-11-11 22:02:02 -0500

    Refactor (foundry-entry): Update javascript and some markup to correct bugs from earlier refactoring.

--- templates/js/app.js
+++ templates/js/app.js
@@ -6,8 +6,8 @@
         $foundryArticleContent = $(".foundryArticle-content");
         $headerTags = $foundryArticleContent.find(":header");
         createDisplayOptions = function() {
-          $(".site-main").before("<div class=\"foundryArticle-displayOptions\"></div>");
-          return $(".foundryArticle-displayOptions").html("<ul class=\"foundryArticle-list\">" + "<li class=\"displayOptions-item\">" + "<a class=\"displayOptions-link displayOptions-selectedLink displayOptionsLink-FullArticle\" href=\"\">Full Article</a>" + "</li>" + "<li class=\"displayOptions-item\">" + "<a class=\"displayOptions-link displayOptionsLink-QuickRead\" href=\"\">Quick Read</a>" + "</li>" + "</ul>");
+          $(".site-main").before("<div class=\"foundry-header foundryArticle-displayOptions\"></div>");
+          return $(".foundryArticle-displayOptions").html("<ul class=\"foundry-header-list foundryArticle-list\">" + "<li class=\"foundry-header-listItem displayOptions-item\">" + "<a class=\"foundry-header-listItem-link foundry-header-listItem-selectedLink displayOptionsLink-FullArticle\" href=\"\">Full Article</a>" + "</li>" + "<li class=\"foundry-header-listItem displayOptions-item\">" + "<a class=\"foundry-header-listItem-link displayOptionsLink-QuickRead\" href=\"\">Quick Read</a>" + "</li>" + "</ul>");
         };
         createIntroText = function() {
           var $introContainer, $introText;
@@ -24,25 +24,25 @@
           var $startContent;
           $startContent = $(".foundryArticle-intro").nextUntil(".foundryArticle-expandableHeader");
           $startContent.wrapAll("<div class=\"foundryArticle-expandableContent\"></div>");
-          $(".foundryArticle-expandableContent").first().before("<div class=\"foundryArticle-expandableHeader foundryArticle-addedHeader\">Introduction</div>");
-          return $(".foundryArticle-expandableHeader").each(function() {
+          $(".foundryArticle-expandableHeader").each(function() {
             return $(this).nextUntil(".foundryArticle-expandableHeader").wrapAll("<div class=\"foundryArticle-expandableContent\"></div>");
           });
+          return $(".foundryArticle-expandableContent").first().before("<div class=\"foundryArticle-expandableHeader foundryArticle-addedHeader\">Introduction</div>");
         };
         return $(document).ready(function() {
           if ($headerTags.length > 0) {
             createDisplayOptions();
-            $(".displayOptions-link").click(function(e) {
-              $(".displayOptions-link").each(function() {
-                return $(this).removeClass("displayOptions-selectedLink");
+            $(".foundry-header-listItem-link").click(function(e) {
+              $(".foundry-header-listItem-link").each(function() {
+                return $(this).removeClass("foundry-header-listItem-selectedLink");
               });
               if ($(this).hasClass("displayOptionsLink-QuickRead")) {
-                $(this).addClass("displayOptions-selectedLink");
+                $(this).addClass("foundry-header-listItem-selectedLink");
                 $foundryArticleContent.removeClass("readingStyle-fullArticle");
                 $foundryArticleContent.addClass("readingStyle-quickRead");
               }
               if ($(this).hasClass("displayOptionsLink-FullArticle")) {
-                $(this).addClass("displayOptions-selectedLink");
+                $(this).addClass("foundry-header-listItem-selectedLink");
                 $foundryArticleContent.removeClass("readingStyle-quickRead");
                 $foundryArticleContent.addClass("readingStyle-fullArticle");
               }
